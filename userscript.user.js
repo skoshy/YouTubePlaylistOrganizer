312f4b7f506482c778bd4e64a10711dbf8fc22fe
Stefan Koshy <skoshy@nextjump.com>
2017-07-12 18:01:45 -0400

    Added new Vibe Guide name to music channel list

--- userscript.user.js
+++ userscript.user.js
@@ -2,7 +2,7 @@
 // @name         YouTube Playlist Organizer
 // @icon         http://i.imgur.com/9fbPeGr.png
 // @namespace    skoshy.com
-// @version      0.1.8
+// @version      0.1.9
 // @description  Allows you to organize playlists on YouTube
 // @author       Stefan Koshy
 // @updateURL    https://raw.githubusercontent.com/skoshy/YouTubePlaylistOrganizer/master/userscript.user.js
@@ -271,6 +271,7 @@ function parseVideoSortName(videoDetails) {
 	'futurism',
 	'majestic casual',
 	'the vibe guide xo',
+	'xo',
 	'headphones recommended',
 	'house nation',
 	'thesoundyouneed',
